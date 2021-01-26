b4120148acadf711f5b63c63cb280ce5bb6485e7
Mayur C Dhage <mdhage139@altimetrik.com>
2021-01-26 15:23:05 +0530

    starting web api

---
+++ src/app.js
@@ -0,0 +1,42 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+
+const app = express()
+
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.send('Hello express!')
+})
+
+app.get('/help', (req, res) => {
+    res.send('Help page')
+})
+
+app.get('/about', (req, res) => {
+    res.send('About')
+})
+
+app.get('/weather', (req, res) => {
+    res.send('Your weather')
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000.')
+})
+
+console.dir(express)
+console.dir(app)
+console.log("express")
